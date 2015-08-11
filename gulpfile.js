d90cb067b200a36637686fbd141b40e51db64d39
Deedar Alam <deedaralam@gmail.com>
2015-08-11 23:29:33 +0500

    Concatenated Javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
+	
+var jsSources = [
+	'components/scripts/jquery.js',
+	'components/scripts/bootstrap.js'
+]	
 
 gulp.task('log',function(){
 	gutil.log('Khabarfeed are news portail website')
 	
+});
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
